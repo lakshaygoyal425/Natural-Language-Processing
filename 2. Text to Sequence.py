1ff4b50d2c1cc891ed89da3e06d2665e214798a1
Lakshay Goyal <47741067+lakshaygoyal425@users.noreply.github.com>
2020-08-20 09:35:27 +0530

    Add files via upload

---
+++ 2. Text to Sequence.py
@@ -0,0 +1,17 @@
+from tensorflow.keras.preprocessing.text import Tokenizer
+
+sentences = [
+    'I love my dog',
+    'I love my cat',
+    'You love my dog!',
+    'Do you think my dog is amazing?'
+    ]
+
+tokenizer = Tokenizer(num_words=100)
+tokenizer.fit_on_texts(sentences)
+word_index = tokenizer.word_index
+
+sequences = tokenizer.texts_to_sequences(sentences)
+
+print(word_index)
+print(sequences)
